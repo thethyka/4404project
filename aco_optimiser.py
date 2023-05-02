c14794aba8c15cdf7a2b2d0c38ba36a0f495edbc
thethyka <thethyka@gmail.com>
2023-05-03 02:05:49 +0800

    Wrote new ACO alg

---
+++ aco_optimiser.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+
+class Ant:
+    def __init__(self):
+        self.buy_pulse_formula = None
+        self.sell_pulse_formula = None
+
+
+class ACOOptimiser:
+    def __init__(self, bot_instance, data, num_literals, num_ants, num_iterations, alpha, beta, evaporation_rate, max_clauses):
+        
+        # Set the parameters for the ACO algorithm
+        self.data = data
+        self.num_literals = num_literals
+        self.num_ants = num_ants
+        self.num_iterations = num_iterations
+        self.alpha = alpha
+        self.beta = beta
+        self.evaporation_rate = evaporation_rate
+        self.max_clauses = max_clauses
+
+        # List of ant objects
+        self.ants = [Ant() for _ in range(self.num_ants)]
+
+        self.pheromone_matrix = None
+        self.bot = bot_instance
+
+    def initialise_pheromone_matrix(self):
+        """
+        Initialize the pheromone matrix with rows representing literals
+        and columns representing positions in clauses.
+        Uses num_literals, num_positions
+        sets pheremone matrix.
+        """
+        # Initialize pheromone matrix with small random values
+        pass
+
+    def generate_random_dnf_formula(self):
+        """
+        Generate a random DNF formula with a random number of clauses within the specified range.
+        Outputs both the sell and buy pulse DNF formulas.
+        Uses num_literals, max_clauses
+        """
+        # Create a random DNF formula
+        pass
+
+    def initialise_ants(self):
+        """
+        Initialize a population of ants with random starting DNF formulas.
+        Uses num_ants, num_literals, min_clauses, max_clauses
+        sets ants.
+        """
+        # Create a list of ants with random DNF formulas
+        pass
+
+    def construct_solution(self, ant):
+        """
+        Refine the DNF formula of an ant based on the pheromone matrix and heuristic information.
+        Uses ant, pheromone_matrix, alpha, beta
+        """
+        # Modify the ant's DNF formula using probabilities derived from the pheromone matrix
+        pass
+
+    def evaluate_solution(self, ant):
+        """
+        Evaluate a DNF formula using the provided cost function.
+        """
+
+        return self.bot.run(ant.buy_pulse_formula, ant.sell_pulse_formula)
+
+
+    def update_pheromone(self, solutions_fitness):
+        """
+        Update the pheromone matrix based on the quality of the solutions found by the ants.
+        Uses pheromone_matrix, ants, solutions_fitness, evaporation_rate
+        """
+        # Update the pheromone levels and apply evaporation
+        pass
+
+    def extract_best_solution(self):
+        """
+        Extract the best DNF formula found by the ants.
+        Uses ants, solutions_fitness
+        """
+        # Find the ant with the highest fitness solution
+        pass
+
+    def check_convergence(self):
+        """
+        Checks if our alg has converged.
+        """
+        
+
+    def aco_algorithm(self):
+        """
+        Run the ACO algorithm for the given number of iterations and return the best DNF formula.
+        Uses num_literals, num_ants, num_iterations, alpha, beta, evaporation_rate, max_clauses
+        """
+
+        # Initialize pheromone matrix
+        pheromone_matrix = self.initialise_pheromone_matrix()
+
+        # Initialize ants
+        ants = self.initialise_ants()
+
+        # Main loop
+        
+        for iteration in range(self.num_iterations):
+            solutions_fitness = []
+            # Construct solutions
+            for ant in ants:
+                self.construct_solution(ant)
+                solutions_fitness.append(self.evaluate_solution(ant))
+            # Evaluate solutions, need to pass in the trading bot instance?
+            
+
+            # Update pheromone levels
+            self.update_pheromone(solutions_fitness)
+
+            if self.check_convergence():
+                break
+
+        # Extract the best solution
+        best_buy_dnf_formula, best_sell_dnf_formula = self.extract_best_solution(ants, solutions_fitness)
+
+        return (best_buy_dnf_formula, best_sell_dnf_formula)
+
+
